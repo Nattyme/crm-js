b66415c95785dd4673f773138a8685462b809505
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-01 13:33:49 +0300

    model: form edit base methods

--- js/edit/edit.view.js
+++ js/edit/edit.view.js
@@ -3,20 +3,56 @@ import { Formatter } from './../utils/formatter.js';
 import FormActions from './../form/FormView/proto/FormActions.js';
 
 class FormEdit extends FormActions {
- constructor (form, select, inputs) {
-  super( form, select, inputs );                      // Вызовем констр-р род. класса
-  this.form = document.querySelector('#form');        // Теперь можно исполз-ть 'this'. Надём форму на текущ. странице            
-  this.select = this.form.querySelector('#status');
+ constructor (form, select, selectStatus, inputs) {
+  super( form, select, selectStatus, inputs );                  // Вызовем констр-р род. класса и передадим ему парам-ры
+
+  this.form = document.querySelector('#form');                // Теперь можно исполз-ть 'this'. Надём форму на текущ. странице            
+  this.select = this.form.querySelector('#product');
+  this.selectStatus = this.form.querySelector('#status');
+
+  this.inputs =  {
+    full_name  : this.form.querySelector('#name'),
+    phone : this.form.querySelector('#phone'),
+    email : this.form.querySelector('#email'),
+  }
+
   this.taskId = this.setTaskId();                     // Установим ID задачи 
  }
 
+ /**
+ * Получает ID задачи из строки запроса URL.
+ * Если ID не найден, выводит сообщение об ошибке.
+ *
+ * @returns {number|null} Возвращает ID задачи, если он найден, или null, если ID не найден.
+ */
  setTaskId () {
-  const formatter = new Formatter();
-  const id = formatter.getUrlID();
-  console.log(id);
-  // console.log(uriLastParam);
-  
+  return this.getTaskID();
  }
-}
 
+ getTaskID () {
+  const formatter = new Formatter();
+
+  // получим и вернём id задачи
+  if ( formatter.getUrlID() !== null ) {
+    return formatter.getUrlID();
+  } else {
+    console.log('Не получен ID задачи'); 
+    return null;
+  }
+ }
+
+ getTask(id) {
+ 
+ }
+
+ setFormTaskValue() {
+  console.log(this);
+  const id = getTaskID();
+  const taskData = getTask(id);
+  console.log(taskData);
+ }
+
+
+}
+// this.setFormData();
 export { FormEdit };
