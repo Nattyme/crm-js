392f192a071b82820427f4b9e044d0f4cb36b820
NeuralNat <82378331+Nattyme@users.noreply.github.com>
2024-12-04 20:29:49 +0300

    view: edit form render new methods to get or set form elements

--- js/edit/EditFormRender.js
+++ js/edit/EditFormRender.js
@@ -14,23 +14,65 @@ class EditFormRender  {
   }
  }
 
- updateNoteWrapper (content) {
-  this.noteWrapper.textContent = content;
- }
+  updateNoteWrapper (content) {
+    this.noteWrapper.textContent = content;
+  }
 
+  getTaskId() {
+    return this.id.value;
+  }
+  setTaskId(id) {
+    this.id = id;
+  }
+
+
+  getDateValue() {
+    return this.date.value;
+  }
+  setDateValue(value) {
+    this.value = value;
+  }
+
+  gitInputValues() {
+    return {
+      full_name : this.full_name,
+      phone : this.phone,
+      email : this.email
+    }
+  }
+  setInputValues( {full_name, phone, email} ) {
+    if (full_name !== undefined) this.inputs.full_name.value = full_name;
+    if (phone !== undefined) this.inputs.phone.value = phone;
+    if (email !== undefined) this.inputs.email.value = email;
+  }
+
+  getStatusValue() {
+    return this.selectStatus.value;
+  }
+  setSatusValue(statusNew) {
+    this.selectStatus.value = statusNew;
+  }
+
+  setFormValues ( {id, date, select, selectStatus, inputs}) {
+    if (id !== undefined) this.setTaskId(id);
+    if (date !== undefined) this.setDateValue(date);
+    if (select !== undefined) this.select.value = select;
+    if (selectStatus !== undefined) this.selectStatus(selectStatus);
+    if (inputs) this.setInputValues(inputs);
+  }
 
  
- getFormElements(){
-  return {
-    form : this.form,
-    noteWrapper : this.noteWrapper,
-    id : this.id,
-    date : this.date,
-    select : this.select,
-    selectStatus : this.selectStatus,
-    inputs : this.inputs
+  getFormElements(){
+    return {
+      form : this.form,
+      noteWrapper : this.noteWrapper,
+      id : this.id,
+      date : this.date,
+      select : this.select,
+      selectStatus : this.selectStatus,
+      inputs : this.inputs
+    }
   }
- }
 
 }
 
